d35191bec1c228a5aadf2396c0fd87cbf60f5ad8
qitas <qitas@qitas.cn>
2021-04-27 17:18:32 +0800

    init

--- examples/arduino-internal-libs/src/ChatServer.ino
+++ examples/arduino-internal-libs/src/ChatServer.ino
@@ -1,22 +1,3 @@
-/*
- Chat  Server
-
- A simple server that distributes any incoming messages to all
- connected clients.  To use telnet to  your device's IP address and type.
- You can see the client's input in the serial monitor as well.
- Using an Arduino Wiznet Ethernet shield.
-
- Circuit:
- * Ethernet shield attached to pins 10, 11, 12, 13
- * Analog inputs attached to pins A0 through A5 (optional)
-
- created 18 Dec 2009
- by David A. Mellis
- modified 9 Apr 2012
- by Tom Igoe
-
- */
-
 #include <Arduino.h>
 #include <SPI.h>
 #include <Ethernet.h>
@@ -42,7 +23,7 @@ void setup() {
   server.begin();
  // Open serial communications and wait for port to open:
   Serial.begin(9600);
-   while (!Serial) {
+  while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
 
